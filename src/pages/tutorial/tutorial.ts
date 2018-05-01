9c7814063f85f9e7ce8e0282d7e15aeb4886a441
Unknown <uquelhas@gmail.com>
2018-05-02 00:08:20 +0200

    Welcome page w/ slides

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -34,7 +34,7 @@ export class TutorialPage {
           {
             title: values.TUTORIAL_SLIDE1_TITLE,
             description: values.TUTORIAL_SLIDE1_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-1.png',
+            image: 'assets/imgs/logo.png',
           },
           {
             title: values.TUTORIAL_SLIDE2_TITLE,
